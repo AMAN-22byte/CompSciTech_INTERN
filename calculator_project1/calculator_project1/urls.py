5d98ba80d89332b1b4cce9191e917a9de8f09c6a
AMAN-22byte <gettoknowaman@gmail.com>
2024-04-29 12:37:20 +0530

    cmt3

--- calculator_project1/calculator_project1/urls.py
+++ calculator_project1/calculator_project1/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+import producer
 from django.urls import path,include
 import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('producer/', include('producer.urls')),
+    path('producer/', producer.urls),
     path('consumer/', include('consumer.urls')),
     path('scheduler/', include('scheduler.urls')),
     path('', include('calculator.urls')),
