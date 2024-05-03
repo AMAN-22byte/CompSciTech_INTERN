873b1debcec2382e326930d8de04799a3e3dcdc6
AMAN-22byte <gettoknowaman@gmail.com>
2024-05-03 12:41:31 +0530

    cmt

--- calcy/urls.py
+++ calcy/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', include('calculator.urls')),  
     path('producer/', include('producer.urls')),  
