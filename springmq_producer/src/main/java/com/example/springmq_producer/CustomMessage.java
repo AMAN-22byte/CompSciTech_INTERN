539ccf494d12b1df3e6fb307e14ab2bd417d20ab
AMAN-22byte <gettoknowaman@gmail.com>
2024-06-07 22:09:51 +0530

    cmt1

--- springmq_producer/src/main/java/com/example/springmq_producer/CustomMessage.java
+++ springmq_producer/src/main/java/com/example/springmq_producer/CustomMessage.java
@@ -1,8 +1,37 @@
 package com.example.springmq_producer;
 
+import java.util.Date;
+
 public class CustomMessage {
     private String messageId;
     private String message;
     private Date messageDate;
 
+    // Constructor
+    public CustomMessage() {}
+
+    // Getters and Setters
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getMessageDate() {
+        return messageDate;
+    }
+
+    public void setMessageDate(Date messageDate) {
+        this.messageDate = messageDate;
+    }
 }
